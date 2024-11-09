fc9dfcc9b1035d7821a66e3a005956ec9a24b4d6
Abdul <ahusaynn@gmail.com>
2024-11-09 14:20:39 +0300

    Initial Commit

---
+++ XTracker/XTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XTrackerApp.swift
+//  XTracker
+//
+//  Created by mac on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct XTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XTrackerTests/XTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  XTrackerTests.swift
+//  XTrackerTests
+//
+//  Created by mac on 09/11/2024.
+//
+
+import Testing
+@testable import XTracker
+
+struct XTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
